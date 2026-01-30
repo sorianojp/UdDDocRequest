2049c7a844c74fa6fee1681a1f1c3bb770779c18
sorianojp <johnpaul.soriano.6197@gmail.com>
2026-01-30 14:25:30 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -42,4 +42,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     });
 });
 
+Route::get('/debug-config', function () {
+    return response()->json([
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+        'memory_limit' => ini_get('memory_limit'),
+        'max_execution_time' => ini_get('max_execution_time'),
+        'php_version' => phpversion(),
+    ]);
+});
+
 require __DIR__.'/settings.php';
