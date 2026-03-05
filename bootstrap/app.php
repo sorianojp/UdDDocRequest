e9c0bb750bdfb40916325bcd87cfdf109e527d7e
sorianojp <johnpaul.soriano.6197@gmail.com>
2026-03-05 11:26:17 +0800

    added course

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
